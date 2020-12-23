ec536a2c16842f3fa87219036eb3d4eb2b93e613
zoglam <anzoor1998@gmail.com>
2020-12-23 04:21:06 +0300

    renamed views

--- polls_app/urls.py
+++ polls_app/urls.py
@@ -7,20 +7,22 @@ app_name = 'polls_app'
 
 urlpatterns = [
     url(r'^auth/', views.auth),
+    url(r'^pass_poll/(?P<id_poll>\d+)', views.pass_poll),
+    url(r'^history/(?P<id_user>\d+)', views.get_history_by_id)
+]
+
+polls = [
+    url(r'^polls/', views.get_polls),
+    url(r'^create_poll/', views.create_poll),
+    url(r'^alter_poll/(?P<id_poll>\d+)', views.alter_poll),
+    url(r'^delete_poll/(?P<id_poll>\d+)', views.delete_poll),
 ]
 
 questions = [
-    url(r'^questions/', views.getQuestions),
-    url(r'^create_question/', views.createQuestion),
-    url(r'^alter_question/(?P<id_question>\d+)', views.alterQuestion),
-    url(r'^delete_question/(?P<id_question>\d+)', views.deleteQuestion),
+    url(r'^create_question/', views.create_question),
+    url(r'^alter_question/(?P<id_question>\d+)', views.alter_question),
+    url(r'^delete_question/(?P<id_question>\d+)', views.delete_question),
 ]
 
-answers = [
-    url(r'^create_answer/', views.createAnswer),
-    url(r'^alter_answer/(?P<id_answer>\d+)', views.alterAnswer),
-    url(r'^delete_answer/(?P<id_answer>\d+)', views.deleteAnswer),
-]
-
+urlpatterns += polls
 urlpatterns += questions
-urlpatterns += answers
